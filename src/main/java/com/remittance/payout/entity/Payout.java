f813e6c945865918f5bba944ee8cc7f6e1e0206a
tolu13 <tolulopeolanrewaju8@gmail.com>
2026-04-24 17:46:46 +0100

    refactor(payout entity): improve entity constraints to manage JPA

--- src/main/java/com/remittance/payout/entity/Payout.java
+++ src/main/java/com/remittance/payout/entity/Payout.java
@@ -1,12 +1,17 @@
 package com.remittance.payout.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Entity
+@NoArgsConstructor
 public class Payout {
 
     @Id
