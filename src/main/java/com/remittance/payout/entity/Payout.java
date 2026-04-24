8fe855c82f9e71e7271b11bd6dfb5c9b29d0c41e
tolu13 <tolulopeolanrewaju8@gmail.com>
2026-04-24 17:30:50 +0100

    feat(payout entity): implement Payout entity with one-to-one remittance mapping and provider reference

--- src/main/java/com/remittance/payout/entity/Payout.java
+++ src/main/java/com/remittance/payout/entity/Payout.java
@@ -1,4 +1,41 @@
 package com.remittance.payout.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Payout {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "remittance_id", nullable = false, unique = true )
+    private Remittance remittance;
+
+    @Column(nullable = false, unique = true)
+    private String providerReference;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+    public enum Status {
+        PENDING,
+        SUCCESS,
+        FAILED
+    }
+
 }
