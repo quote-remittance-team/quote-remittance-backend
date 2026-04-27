bdf799a4886ac1d5c5f2d8e73567ea424f2fa371
ayodele <kadelcode@gmail.com>
2026-04-27 17:13:20 +0100

    feature(user): create user repository (#29)

---
+++ src/main/java/com/remittance/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.remittance.user.repository;
+
+import com.remittance.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
