ef3c5723a9336fb3926e4c2fe473bc8696002d68
hibeenovul <hibeenovul19@gmail.com>
2026-04-28 00:00:49 +0100

    feat(#37): implement DepositService with initiation, webhook handling, and status updates

---
+++ src/main/java/com/remittance/deposit/service/DepositService.java
@@ -0,0 +1,49 @@
+package com.remittance.deposit.service;
+
+import com.remittance.deposit.entity.Deposit;
+import com.remittance.deposit.repository.DepositRepository;
+import com.remittance.enums.DepositStatus;
+import com.remittance.quote.entity.Quote;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+
+public class DepositService {
+    private final DepositRepository depositRepository;
+
+    @Transactional
+    public Deposit initiateDeposit(Quote quote, BigDecimal amount, String currency, String idempotencyKey) {
+        Deposit deposit =  Deposit.builder()
+                .quote(quote)
+                .amount(amount)
+                .currency(currency)
+                .status(DepositStatus.PENDING)
+                .idempotencyKey(idempotencyKey)
+                .build();
+
+        return depositRepository.save(deposit);
+    }
+    @Transactional
+    public Deposit handlePaymentCallback(String paymentReference, DepositStatus newStatus) {
+        Deposit deposit = depositRepository.findByPaymentReference(paymentReference)
+                .orElseThrow(() -> new IllegalArgumentException("Deposit not found for reference:" + paymentReference));
+
+        deposit.setStatus(newStatus);
+        return depositRepository.save(deposit);
+    }
+
+    @Transactional
+    public Deposit updateDepositStatus(UUID depositId, DepositStatus newStatus) {
+        Deposit deposit = depositRepository.findById(depositId)
+                .orElseThrow(() -> new IllegalArgumentException("Deposit not found for id:" + depositId));
+
+        deposit.setStatus(newStatus);
+        return depositRepository.save(deposit);
+    }
+}
