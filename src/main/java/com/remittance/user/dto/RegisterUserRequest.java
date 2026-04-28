66859b0967f372fef80427d84d65572075f05ace
ayodele <kadelcode@gmail.com>
2026-04-28 23:07:13 +0100

    refactor(user-service): add field validations to api boundary (#35)

--- src/main/java/com/remittance/user/dto/RegisterUserRequest.java
+++ src/main/java/com/remittance/user/dto/RegisterUserRequest.java
@@ -1,5 +1,7 @@
 package com.remittance.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,7 +9,10 @@ import lombok.Getter;
 @Builder
 public class RegisterUserRequest {
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
 
+    @NotBlank(message = "Password is required")
     private String password;
 }
