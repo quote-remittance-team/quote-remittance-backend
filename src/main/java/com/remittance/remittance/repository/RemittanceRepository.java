2c509e1a815ef929209e55ab53994dbe97ef5c56
ayodele <kadelcode@gmail.com>
2026-04-28 11:36:44 +0100

    feature(remittance): implement remittance repository (#32)

---
+++ src/main/java/com/remittance/remittance/repository/RemittanceRepository.java
@@ -0,0 +1,19 @@
+package com.remittance.remittance.repository;
+
+import com.remittance.remittance.entity.Remittance;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RemittanceRepository extends JpaRepository<Remittance, UUID> {
+
+    Optional<Remittance> findByReference(String reference);
+
+    Optional<Remittance> findByDeposit_Id(UUID depositId);
+
+    Page<Remittance> findBySender_Id(UUID senderId, Pageable pageable);
+
+}
