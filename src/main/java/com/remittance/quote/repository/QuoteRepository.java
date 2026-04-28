e719dc4f4cbc371d6482de7d014836dc03708e27
ayodele <kadelcode@gmail.com>
2026-04-28 10:48:50 +0100

    refactor(quote): modify quote repository to support finding quote by quoteReference (#30)

--- src/main/java/com/remittance/quote/repository/QuoteRepository.java
+++ src/main/java/com/remittance/quote/repository/QuoteRepository.java
@@ -1,15 +1,18 @@
 package com.remittance.quote.repository;
 
 import com.remittance.quote.entity.Quote;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface QuoteRepository extends JpaRepository<Quote, UUID> {
 
-    List<Quote> findByUser_Id(UUID userId);
+    Page<Quote> findByUser_Id(UUID userId, Pageable pageable);
 
-    Optional<Quote> findByIdAndUserId(UUID id, UUID userId);
+    Optional<Quote> findByIdAndUser_Id(UUID id, UUID userId);
+
+    Optional<Quote> findByQuoteReference(String quoteReference);
 }
