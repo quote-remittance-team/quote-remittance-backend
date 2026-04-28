5ccf33fb1408cca940d8941f97eb400bf3c8ec6b
ayodele <kadelcode@gmail.com>
2026-04-28 23:28:54 +0100

    refactor(user-service): update `RegisterUserRequest` dto for proper JSON deserialization (#35)

--- src/main/java/com/remittance/user/dto/RegisterUserRequest.java
+++ src/main/java/com/remittance/user/dto/RegisterUserRequest.java
@@ -2,11 +2,15 @@ package com.remittance.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@Builder
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterUserRequest {
 
     @NotBlank(message = "Email is required")
