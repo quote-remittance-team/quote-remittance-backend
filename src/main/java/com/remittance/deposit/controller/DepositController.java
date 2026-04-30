e256bdf9db5e757ec8997333b1e872516be8a855
hibeenovul <hibeenovul19@gmail.com>
2026-04-30 16:26:08 +0100

    feat(#45): implement Notification controller and tessting

--- src/main/java/com/remittance/deposit/controller/DepositController.java
+++ src/main/java/com/remittance/deposit/controller/DepositController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -28,16 +27,16 @@ public class DepositController {
 
     // POST/deposits
     @PostMapping
-    public ResponseEntity<Deposit>createDeposit(@Valid  @RequestBody DepositRequestDto request) {
+    public ResponseEntity<Deposit> createDeposit(@Valid  @RequestBody DepositRequestDto request) {
         log.info("Received request to initiate deposit for quote ID {}", request.getQuoteId());
         Deposit createdDeposit = depositService.initiateDeposit(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdDeposit);
     }
     // POST /deposits/webhook
     @PostMapping("/webhook")
-    public ResponseEntity<Deposit>handleWebhook(@Valid @RequestBody DepositWebhookDto webhookPayload, @RequestHeader(value = "X-Provider-Signature", required = false) String providedSignature) {
+    public ResponseEntity<Deposit> handleWebhook(@Valid @RequestBody DepositWebhookDto webhookPayload, @RequestHeader(value = "X-Provider-Signature", required = false) String providedSignature) {
         log.info("Received webhook for payment reference: {} with status: {}", webhookPayload.getPaymentReference(), webhookPayload.getStatus());
-        if (providedSignature == null || ! providedSignature.equals(expectedWebhookSecret)) {
+        if (providedSignature == null || !java.security.MessageDigest.isEqual(providedSignature.getBytes(java.nio.charset.StandardCharsets.UTF_8),expectedWebhookSecret.getBytes(java.nio.charset.StandardCharsets.UTF_8))) {
             log.warn("SECURITY ALERT: Unauthorized webhook spoofing attempt blocked!");
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid webhook spoofing attempt blocked!");
         }
