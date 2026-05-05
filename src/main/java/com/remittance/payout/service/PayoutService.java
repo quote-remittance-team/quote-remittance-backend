395893a8bf885eed52333e38c0166bb4bcc5aa3b
hibeenovul <hibeenovul19@gmail.com>
2026-05-05 14:20:40 +0100

    feat(#39): implement payout service orchestration and unit tests

--- src/main/java/com/remittance/payout/service/PayoutService.java
+++ src/main/java/com/remittance/payout/service/PayoutService.java
@@ -1,8 +1,40 @@
 package com.remittance.payout.service;
 
-import com.remittance.remittance.entity.Remittance;
+import com.remittance.enums.PayoutStatus;
+import com.remittance.integration.payout.PayoutClient;
+import com.remittance.integration.payout.dto.TransferRequest;
+import com.remittance.integration.payout.dto.TransferResponse;
+import com.remittance.payout.entity.Payout;
+import com.remittance.payout.repository.PayoutRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.UUID;
 
-public interface PayoutService {
+@Service
+public class PayoutService {
+    private final PayoutRepository payoutRepository;
+    private final PayoutClient payoutClient;
 
-    void triggerPayout(Remittance remittance);
+    public PayoutService(PayoutRepository payoutRepository, PayoutClient payoutClient) {
+        this.payoutRepository = payoutRepository;
+        this.payoutClient = payoutClient;
+    }
+    @Transactional
+    public void processPayout(UUID remittanceId, String recipientCode, long amountInKobo) {
+        Payout payout = payoutRepository.findByRemittanceId(remittanceId).orElseThrow(() -> new IllegalArgumentException("No pending payout found for Remittance Id: " + remittanceId));
+        TransferRequest request = new TransferRequest(
+                "balance",
+                amountInKobo,
+                recipientCode,
+                "Payout for Remittance: " + remittanceId.toString()
+        );
+        TransferResponse response = payoutClient.initiateTransfer(request);
+        if (response.status()) {
+            payout.setProviderReference(response.data().transferCode());
+            payout.setStatus(PayoutStatus.COMPLETED);
+        } else {
+            payout.setStatus(PayoutStatus.FAILED);
+        }
+        payoutRepository.save(payout);
+    }
 }
