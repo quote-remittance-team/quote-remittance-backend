edc76ec23cd1bb3296b9f7813ce4cae5568af994
hibeenovul <hibeenovul19@gmail.com>
2026-05-20 11:33:26 +0100

    fix(#97): changed system print to production grade application

--- src/main/java/com/remittance/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/com/remittance/auth/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +18,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private static final String BEARER_PREFIX = "Bearer ";
@@ -47,7 +49,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         try {
             userEmail = jwtService.extractEmail(jwt);
         } catch (io.jsonwebtoken.ExpiredJwtException e) {
-            System.out.println("JWT expired, Passing request as Unauthenticated");
+            log.debug("JWT expired, Passing request as Unauthenticated");
         }
 
         if (userEmail != null
