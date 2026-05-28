f3b1183516ce4eef9bd708ef13ebc85e6929c6a0
ayodele <kadelcode@gmail.com>
2026-05-28 19:36:37 +0100

    refactor(CORS): remove hardcoded cors allowed origins and remove WebMvcConfigurer (#108)

--- src/main/java/com/remittance/config/CorsConfig.java
+++ src/main/java/com/remittance/config/CorsConfig.java
@@ -1,20 +1,33 @@
 package com.remittance.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
 
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173"
-                )
-                .allowedMethods(
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration =
+                new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+                Arrays.asList(allowedOrigins)
+        );
+
+        configuration.setAllowedMethods(
+                List.of(
                         "GET",
                         "POST",
                         "PUT",
@@ -22,7 +35,22 @@ public class CorsConfig implements WebMvcConfigurer {
                         "DELETE",
                         "OPTIONS"
                 )
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        );
+
+        configuration.setAllowedHeaders(
+                List.of("*")
+        );
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+        );
+
+        return source;
     }
 }
