c189a9bad2097676ba7f585f9609646643d7f7c7
tolu13 <tolulopeolanrewaju8@gmail.com>
2026-06-16 10:06:45 +0100

    fixed bypassing from spring security anonymousUser

--- src/test/java/com/remittance/quote/controller/QuoteControllerTest.java
+++ src/test/java/com/remittance/quote/controller/QuoteControllerTest.java
@@ -44,7 +44,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(QuoteController.class)
-@AutoConfigureMockMvc(addFilters = false)
 class QuoteControllerTest {
 
     @Autowired
